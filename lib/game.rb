cc515ec2adb0e6a6f0212c7716b1942e06b518be
Diego Suarez and Margie Nevarez <margie@>
2016-12-01 15:08:10 -0800

    initiate game class; create deck of cards; deal cards

---
+++ lib/game.rb
@@ -0,0 +1,37 @@
+require('pry')
+class Game
+  @@deck=[]
+  define_method(:initialize) do
+
+  end
+  define_singleton_method(:build_deck) do
+
+    ranks=["A","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+    suits=["Hearts","Spades","Diamond","Clubs"]
+    suits.each do |suit|
+      ranks.each do |rank|
+        @@deck.push(rank+" "+suit)
+      end
+    end
+  end
+
+  define_singleton_method(:deck) do
+    @@deck
+  end
+
+  define_singleton_method(:shuffle) do
+    @@deck.shuffle!()
+  end
+
+  define_singleton_method(:deal) do
+    deal = @@deck.take(5)
+    @@deck.slice!(0..4)
+    deal
+  end
+
+end
+
+Game.build_deck()
+Game.shuffle()
+p Game.deal()
+p Game.deck.length
---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('game')
+
+describe(Game) do
+  describe ('#build_deck') do
+    it('will build the deck') do
+      Game.build_deck()
+      expect(Game.deck()).to(eq(Game.deck()))
+    end
+  end
+end
